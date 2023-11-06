d3533fb0f91974cd2c647eb558b67ff06bffa977
l1nes8 <egor1glavatyje@gmail.com>
2023-11-06 21:52:54 +0200

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Button, ButtonGroup } from '@chakra-ui/react';
 
 // import { logOutThunk } from 'redux/authReducer';
